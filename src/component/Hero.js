cc9eb57456036b25a003e258ac9e0bc21845f188
irfan-fauzi <irfanwintech@yahoo.com>
2021-01-13 21:51:42 +0700

    coba deploy

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+
 function Hero() {
   return (
     <section className="hero container">
-      
-        <img src="https://raw.githubusercontent.com/irfan-fauzi/Shortly-landing/6caa3eb177df56a4f17c60e6bfcddabe00030be0/images/illustration-working.svg" alt="working" className="workImg"></img>
-        <div className="hero-text-wrap">
-          <h1 className="my-1">More than just shorter links</h1>
-          <p className="my-1">Build your brand's recognition and get detailed insights on how your links are performing</p>
-          <button className="btn">Get Started</button>
-        </div>
-      
+
+      <img src="/images/illustration-working.svg" alt="working" className="workImg"></img>
+      <div className="hero-text-wrap">
+        <h1 className="my-1">More than just shorter links</h1>
+        <p className="my-1">Build your brand's recognition and get detailed insights on how your links are performing</p>
+        <button className="btn">Get Started</button>
+      </div>
+
     </section>
   )
 }
